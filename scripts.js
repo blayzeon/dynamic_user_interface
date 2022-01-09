960528fd00c0a2af0a3d49c3f897788259715b38
blayzeon <blayzeon@gmail.com>
2022-01-09 13:43:52 -0500

    Added material icon support

--- scripts.js
+++ scripts.js
@@ -1,19 +1,21 @@
-const divContentDemo = [['<a href="#" "target="_blank">This is a link.</a>', 'This is not.'], ['These options are created with nested arrays.', 'You can add as many options as you would like.', 'Or as few as you would like.', 'This is this line is on index [1][3].'], ['The array items can be any length.']];
-const menuBtnsDemo = ['Services', 'Contact', 'Account', 'Services', 'Contact', 'Account', 'Services', 'Contact', 'Account'];
-
-const menus = function generateDropDownMenus(divContent, menuBtns, limit = 3, container = document.body) {
+const menus = function generateDropDownMenus(dArray, lArray, limit = ['none'], container = document.body) {
   const dropdownUl = document.createElement('ul');
   dropdownUl.classList.add('dropdown-menu');
 
   // in case of overflow
   const dropdownExtra = document.createElement('li');
   dropdownExtra.classList.add('dropdown-menu');
-
   const dropdownExtraBtn = document.createElement('button');
   dropdownExtraBtn.classList.add('dropdown-menu');
-  dropdownExtraBtn.innerText = '≡';
+  if (limit[0] !== 'none') {
+    if (limit.length === 1) {
+      dropdownExtraBtn.innerText = '≡';
+    } else {
+      // if you wish to use material icons, add a limit of [number, icon-name]
+      dropdownExtraBtn.innerHTML = `<span class="material-icons">${limit[1]}</span>`;
+    }
+  }
   dropdownExtra.appendChild(dropdownExtraBtn);
-
   const dropdownExtraDiv = document.createElement('div');
   dropdownExtraDiv.classList.add('dropdown-menu');
   dropdownExtra.appendChild(dropdownExtraDiv);
@@ -21,45 +23,57 @@ const menus = function generateDropDownMenus(divContent, menuBtns, limit = 3, co
   dropdownExtraDiv.appendChild(dropdownExtraDivUl);
 
   // creates the menu items
-  for (let i = 0; i < menuBtns.length; i += 1) {
+  for (let i = 0; i < lArray.length; i += 1) {
     const dropdownLi = document.createElement('li');
     dropdownLi.classList.add('dropdown-menu');
-
+    const dropdownIconInfo = document.createElement('div');
+    /*
+    Absolute positioning makes this disappear - TODO: Program hover menu for this text.
+    dropdownIconInfo.classList.add('dropdown-menu');
+    */
     const dropdownBtn = document.createElement('button');
     dropdownBtn.classList.add('dropdown-menu');
-    dropdownBtn.innerText = menuBtns[i];
+    if (lArray[i].length === 2) {
+      dropdownBtn.innerHTML = `<span class="material-icons">${lArray[i][1]}</span>`;
+      dropdownIconInfo.innerText = `${lArray[i][0]}`;
+    } else {
+      dropdownBtn.innerText = lArray[i];
+    }
 
     const dropdownDiv = document.createElement('div');
     dropdownDiv.classList.add('dropdown-menu');
-    let divContentItems = '';
-    if (divContentItems[0] === undefined) {
-      divContentItems = '<li>There is nothing to display</li>';
+    let dArrayItems = '';
+    if (dArray[i] === undefined) {
+      dArrayItems = '<li>There is nothing to display</li>';
+    } else if (dArray.length === 1) {
+      dArrayItems += `<li>${dArray[i]}</li>`;
     } else {
-      for (let j = 0; j < divContent[i].length; j += 1) {
-        divContentItems += `<li>${divContent[i][j]}</li>`;
+      for (let j = 0; j < dArray[i].length; j += 1) {
+        dArrayItems += `<li>${dArray[i][j]}</li>`;
       }
     }
-    dropdownDiv.innerHTML = `<ul>${divContentItems}</ul>`;
+    dropdownDiv.innerHTML = `<ul>${dArrayItems}</ul>`;
 
     // append the children
     dropdownLi.appendChild(dropdownBtn);
     dropdownLi.appendChild(dropdownDiv);
 
-    if (i < limit) {
+    if (limit[0] === 'none' || i < limit[0]) {
       dropdownUl.appendChild(dropdownLi);
+      dropdownUl.appendChild(dropdownIconInfo);
     } else {
       dropdownExtraDivUl.appendChild(dropdownLi);
     }
   }
   // adds the generated menu to the specified element container, usually a nav
-  if (menuBtns.length > limit) {
+  if (limit[0] !== 'none' && lArray.length > limit[0]) {
     dropdownUl.appendChild(dropdownExtra);
   }
   container.appendChild(dropdownUl);
 
   // add the event listener to make it work
   document.addEventListener('click', (e) => {
-    const dropdownBtn = e.target.matches('button.dropdown-menu');
+    const dropdownBtn = e.target.matches('button.dropdown-menu') || e.target.matches('button.dropdown-menu > span');
     if (!dropdownBtn && e.target.closest('.dropdown-menu') != null) return;
 
     let currentMenu;
@@ -75,5 +89,9 @@ const menus = function generateDropDownMenus(divContent, menuBtns, limit = 3, co
   });
 };
 
-menus(divContentDemo, menuBtnsDemo);
+const dArrayDemo = [['<a href="#" "target="_blank">This is a link.</a>', '<button>This is a button.</button>', 'This is just text without a tag.'], ['These options are created with nested arrays.', 'You can add as many options as you would like.', 'Or as few as you would like.', 'This is this line is on index [1][3].'], ['The array items can be any length.']];
+const lArrayDemo = [['Services', 'shopping_cart'], ['Contact', 'question_answer'], ['Account', 'account_circle'], 'Services', 'Contact', 'Account', 'Services', 'Contact', 'Account'];
+
+// menus(['test'], [['test', 'menu']], [3, 'menu']);
+menus(dArrayDemo, lArrayDemo, [3, 'menu']);
 // * end of module * //
